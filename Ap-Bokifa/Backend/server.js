889c26b4bd03da6f713ab14aea503bb2cc3a209f
Mihir Makwana <makwanamihir02@gmail.com>
2025-12-04 09:33:51 +0530

    backend initial

---
+++ Ap-Bokifa/Backend/server.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import mongoose from "mongoose";
+import app from "./app.js";
+
+const PORT = process.env.PORT || 4000;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+      console.log(`http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Failed to connect to MongoDB", error);
+  });
